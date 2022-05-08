00b5ff412bae3ca8c1b181f6ebd69928b921c25a
zhangzhibang0309 <295854713@qq.com>
2022-05-08 16:34:14 +0800

    concat

--- type-challenges/easy/533_concat/template.ts
+++ type-challenges/easy/533_concat/template.ts
@@ -1,1 +1,8 @@
-type Concat<T extends any[], U extends any[]> = [...T,...U]
+// 这个比较简单，需要注意的就是最好别用any，这里可以用unknown代替
+type Concat<T extends unknown[], U extends unknown[]> = [...T,...U]
+
+// 两道拓展，使用infer和spread实现
+// 1.取数组第一个
+type FirstArr<T extends unknown[]> = T extends [infer first,...infer rest] ? first : never
+// 2.取数组最后一个
+type LastArr<T extends unknown[]> = T extends [...infer rest,infer last] ? last : never
